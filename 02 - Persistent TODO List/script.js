4545a9301b91e2cf6d35d78662a0cd826e4a2e88
DavidMarkNjogu <davidmarknjogu@gmail.com>
2025-05-02 18:17:50 +0300

    Implemented input functionality where values that are input will be displayed in the ul, given that the user doesn't refresh the page

--- 02 - Persistent TODO List/script.js
+++ 02 - Persistent TODO List/script.js
@@ -2,17 +2,32 @@ const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
-let lists = [
-    {
+let lists = [{
         id: 1,
         name: 'todo1'
-    },
-    {
+},  {
         id: 2,
         name: 'todo2'
-    }
-]
+}]
 
+
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault() //prevents the form from submitting when you press Enter, since it refreshes the page
+    const listName = newListInput.value
+    if (listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
+
+function createList(name){
+    return {id: Date.now().toString(), name: name, tasks: []}
+    
+}
+
+// Creates a new list entry based on the data from the lists array above
 function render() {
     clearElement(listsContainer)
     lists.forEach(list =>{
@@ -24,6 +39,7 @@ function render() {
     })
 }
 
+// Clears any existing inputs
 function clearElement(element){
     while(element.firstChild){
         element.removeChild(element.firstChild)
