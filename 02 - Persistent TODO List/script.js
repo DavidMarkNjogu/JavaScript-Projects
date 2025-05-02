5ad9b7e1ce52fdefe279f5716644f2fbf380ff0a
DavidMarkNjogu <davidmarknjogu@gmail.com>
2025-05-02 19:42:02 +0300

    Added selected list item syntax which highlights it and persists even after refreshing

--- 02 - Persistent TODO List/script.js
+++ 02 - Persistent TODO List/script.js
@@ -3,9 +3,16 @@ const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.SelectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
-
+listsContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'li'){
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
 //Caters for the functionality of the input field
 newListForm.addEventListener('submit', e => {
     e.preventDefault() //prevents the form from submitting when you press Enter, since it refreshes the page
@@ -29,6 +36,8 @@ function saveAndRender(){
 //Saves list to local storage for retrieval after refereshing
 function save(){
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+
 
 }
 // Creates a new list entry based on the data from the lists array above
@@ -39,8 +48,11 @@ function render() {
         listElement.dataset.listId = list.id
         listElement.classList.add("list-name")
         listElement.innerText = list.name
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement) 
-    })
+    })   
 }
 
 // Clears any existing inputs
